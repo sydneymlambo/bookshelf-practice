f261d96163486fa20c18e63d750e62cfc07420f5
Sydney <sydney@artificial.studio>
2021-04-16 11:47:47 +0200

    schema sertup

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
 
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
